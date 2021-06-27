99b493da05c4f168aa5d0b07ef9258ae6bd531d8
jcharfauros <julia.charfauros@gmail.com>
2021-06-26 20:56:33 -0400

    completed

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const key = 'KYqTtPHmCW4ZDAMpTrGagz9eJfHNOxDe900Dffco';
 const card = document.querySelector('.card');
 const cardBody = document.querySelector('.card-body');
 const moreInfoBtn = document.querySelector('.btn');
+const modalBody = document.querySelector('.modal-body')
+const modalTitle = document.querySelector('.modal-title');
 
          //fetching             
 fetch(`${baseURL}?api_key=${key}`)
@@ -20,12 +22,26 @@ function displayImage(spaceObject) {
     let title = document.createElement('h1');
     title.className = 'card-title';
     title.innerText = spaceObject.title; //or spaceObject['title']
+    title.style.fontFamily = 'space-age';
+    // title.style = 'font-family: space-age'; ;
 
     let date = document.createElement('p');
     date.className = 'card-text';
     date.innerText = spaceObject.date;
+    date.style.fontFamily = 'kiona-regular';
+
+    let expl = document.createElement('p');
+    expl.innerText = spaceObject.explanation;
+    expl.style.fontFamily = 'kiona-regular';
+
+    modalTitle.innerText = spaceObject.title;
+    modalTitle.style.fontFamily = 'space-age';
+
+    moreInfoBtn.style.fontFamily = 'kiona-regular';
+
     //order matters!
     card.insertBefore(img, cardBody);
     cardBody.insertBefore(title, moreInfoBtn);
     cardBody.insertBefore(date, moreInfoBtn);
+    modalBody.appendChild(expl);
 }
